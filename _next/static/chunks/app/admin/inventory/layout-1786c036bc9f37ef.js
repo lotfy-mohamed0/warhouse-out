6ebd2003ea8fe00e45adf0db67d01b61ee0d98e0
Lotfy Mohamed Lotfy <lotfy.mohamed0@outlook.com>
2024-08-20 11:31:16 +0300

    Add

---
+++ _next/static/chunks/app/admin/inventory/layout-1786c036bc9f37ef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9951],{9653:function(n,e,u){Promise.resolve().then(u.t.bind(u,2798,23))},2798:function(){}},function(n){n.O(0,[9117,2971,8069,1744],function(){return n(n.s=9653)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/admin/inventory/type/layout-752246e35f2a6bc6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2489,1611,8188,7739,3657,1059,2058,3499,2199,6869,6566,5238,8014,7009,905,4915,8337],{3521:function(){}},function(n){n.O(0,[2971,8069,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
